b4f6056511b65fff6350a6bc641f9c04f73898dd
Sardor095 <60728653+Sardor095@users.noreply.github.com>
2020-03-11 15:49:12 -0700

    Add files via upload

---
+++ ziyoev_sardor_assignment5/beginning-9/js/01-arrays-practice.js
@@ -0,0 +1,89 @@
+//STEP 1
+// var Movies = ["Mr & Mrs Smith", "Kill Bill", "James Bond", "Last Samuray", "Star Wars"];
+
+// console.log(Movies[1]);
+
+//STEP 2
+
+
+//STEP 3
+
+//STEP 4
+
+//STEP 5
+
+//STEP 6
+
+//STEP 7
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
+
+//STEP 11
+
+//STEP 12
+
+//STEP 13
+
+//STEP 14
+
+//STEP 15
+
+//STEP 16
+
+var movies;
+movies = [['Avangers', 1], ['Captine America', 2],['Black Widow', 3],['Knives Out', 4], ['Riddick', 5]];
+
+console.log(movies[0]);
+//STEP 17
+
+// var employe = ["Zak", " Jessica", " Mark", " Fred", " Sally"];
+
+// function showEmployee() {
+
+//     console.log("Employees: " + employe);
+// }
+// console.log(showEmployee());
+
+//STEP 18
+
+// function filt(sar) {
+//     sar = sar.filter(isEligible);
+//     return sar;
+// }
+
+// function isEligible(value) {
+//     if(value !== false || value !== null || value !== 0 || value !== "") {
+//         return value;
+//     }
+// }
+
+// console.log(filt([58, '', 'abcd', true, null, false, 0]));
+
+//STEP 19
+
+// function any_item(items) {
+//     return items[Math.floor(Math.random()*items.length)];
+// }
+
+// const items = [134, 356,355,245,90,34,567];
+// console.log(any_item(items));
+
+//STEP 20
+
+// const numbers = [1,2,67,78,3,4,5,6,7];
+
+// const largestNumber = (values) => {
+//     let highest = 0;
+//     for (let i = 0; i < values.length; i+=1) {
+//         if (values[i] > highest) {
+//             highest = values[i];
+//         }
+//     }
+//     return highest;
+// }
+
+// console.log(largestNumber(numbers));
